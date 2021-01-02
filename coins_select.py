5c0f2ea72f578342451c82cd330a9047c21a8020
Tina-ZJ <15220233520@163.com>
2021-01-02 12:42:08 +0800

    update coins select

--- coins_select.py
+++ coins_select.py
@@ -3,6 +3,11 @@ import sys
 
 
 def coins_way(n):
+	'''
+		给定4种硬币[1,5,10,25]，求数量为n由这4种硬币组合形式有几种方式
+		比如n=5，则由5个1或者1个5的硬币组合而成，所以总共两种方式
+		动态规划求解
+	'''
 	coins = [1, 5, 10, 25]
 	# dp[i][j]表示前i个coin组成j值的方式数量
 	dp = [[0 for _ in range(n+1)] for _ in range(len(coins)+1)]
@@ -19,4 +24,4 @@ def coins_way(n):
 
 
 if __name__=='__main__':
-	print(coins_way(100)) 
+	print(coins_way(5)) 
